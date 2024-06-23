409604abb5365f984acd1be240b4b7cea385e23c
Dipanker Sharma <dipankar.kr2005@gmail.com>
2024-06-23 21:58:50 +0530

    progressing....

--- src/Component/main/Main.jsx
+++ src/Component/main/Main.jsx
@@ -1,52 +1,90 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./main.css";
+import { Context } from "../../context/context";
 
 function Main() {
+  const {
+    onSent,
+    recentPrompt,
+    showResults,
+    loading,
+    resultData,
+    input,
+    setInput,
+  } = useContext(Context);
   return (
     <div className="flex-1 min-h-[100vh] pb-[15vh] relative">
       <div className="nav flex items-center justify-between text-[22px] p-[20px] text-[#585858]">
         <p>Gemini</p>
-        <i className="ri-account-circle-fill "></i>
+        <i className="ri-account-circle-fill text-[30px]"></i>
       </div>
 
       <div className="main max-w-[900px] m-auto">
-        <div className="greet my-[50px] text-[56px] text-[#c4c7c5] font-semibold p-[15px]">
-          <p>
-            <span className="">Hello, Dev</span>
-          </p>
-          <p>How Can I help you today</p>
-        </div>
-
-        <div className="cards flex gap-[15px] p-[20px] ">
-          <div className="card h-[200px] p-[15px] bg-[#f0f4f9] rounded-[10px] relative cursor-pointer hover:bg-[#dfe4ea]">
-            <p className="text-[#585858] text-[17px]">
-              suggest beautiful places to see on an upcoming road trip
-            </p>
-            <i class="ri-compass-discover-line w-[40px] p-[3px] absolute bg-white rounded-full bottom-[10px] right-[10px] text-2xl text-center"></i>
+        {!showResults ? (
+          <>
+            {" "}
+            <div className="greet my-[50px] text-[56px] text-[#c4c7c5] font-semibold p-[12px]">
+              <p>
+                <span className="">Hello, Dev</span>
+              </p>
+              <p>How Can I help you today</p>
+            </div>
+            <div className="cards flex gap-[15px] p-[20px] ">
+              <div className="card h-[200px] p-[15px] bg-[#f0f4f9] rounded-[10px] relative cursor-pointer hover:bg-[#dfe4ea]">
+                <p className="text-[#585858] text-[17px]">
+                  suggest beautiful places to see on an upcoming road trip
+                </p>
+                <i class="ri-compass-discover-line w-[40px] p-[3px] absolute bg-white rounded-full bottom-[10px] right-[10px] text-2xl text-center"></i>
+              </div>
+              <div className="card h-[200px] p-[15px] bg-[#f0f4f9] rounded-[10px] relative cursor-pointer  hover:bg-[#dfe4ea]">
+                <p className="text-[#585858] text-[17px]">
+                  Briefly summarize this concept: urban planning
+                </p>
+                <i class="ri-lightbulb-flash-fill w-[40px] p-[3px] absolute bg-white rounded-full bottom-[10px] right-[10px] text-2xl text-center"></i>
+              </div>
+              <div className="card h-[200px] p-[15px] bg-[#f0f4f9] rounded-[10px] relative cursor-pointer  hover:bg-[#dfe4ea]">
+                <p className="text-[#585858] text-[17px]">
+                  Brainstrom team bonding activities for our work retreat
+                </p>
+                <i class="ri-chat-4-line w-[40px] p-[3px] absolute bg-white rounded-full bottom-[10px] right-[10px] text-2xl text-center"></i>
+              </div>
+              <div className="card h-[200px] p-[15px] bg-[#f0f4f9] rounded-[10px] relative cursor-pointer  hover:bg-[#dfe4ea]">
+                <p className="text-[#585858] text-[17px]">
+                  Improve the readability of the following code
+                </p>
+                <i class="ri-code-s-slash-line w-[40px] p-[3px] absolute bg-white rounded-full bottom-[10px] right-[10px] text-2xl text-center"></i>
+              </div>
+            </div>
+          </>
+        ) : (
+          <div className="result px-[5%] max-h-[70vh] overflow-y-scroll">
+            <div className="result-title my-[0px] flex items-center gap-[20px]">
+              <i className="ri-account-circle-fill text-[30px]"></i>
+              <p>{recentPrompt}</p>
+            </div>
+            <div className="result_data flex items-start gap-[15px] p-1">
+              <i className="ri-bard-fill text-xl"></i>
+              {loading ? (
+                <div className="loader w-[100%] flex flex-col gap-[10px] animation-[pulse_3s_infinite_linear]">
+                  <hr className="rounded-[4px] border-none bg-[#f6f7f8] bg-gradient-to-r from-[#9ed7ff] via-[#ffffff] to-[#9ed7ff] bg-[length:800px_50px] h-[20px]" />
+                  <hr className="rounded-[4px] border-none bg-[#f6f7f8] bg-gradient-to-r from-[#9ed7ff] via-[#ffffff] to-[#9ed7ff] bg-[length:800px_50px] h-[20px]" />
+                  <hr className="rounded-[4px] border-none bg-[#f6f7f8] bg-gradient-to-r from-[#9ed7ff] via-[#ffffff] to-[#9ed7ff] bg-[length:800px_50px] h-[20px]" />
+                </div>
+              ) : (
+                <p
+                  className="text-[17px] leading-[1.8] font-[300]"
+                  dangerouslySetInnerHTML={{ __html: resultData }}
+                ></p>
+              )}
+            </div>
           </div>
-          <div className="card h-[200px] p-[15px] bg-[#f0f4f9] rounded-[10px] relative cursor-pointer  hover:bg-[#dfe4ea]">
-            <p className="text-[#585858] text-[17px]">
-              Briefly summarize this concept: urban planning
-            </p>
-            <i class="ri-lightbulb-flash-fill w-[40px] p-[3px] absolute bg-white rounded-full bottom-[10px] right-[10px] text-2xl text-center"></i>
-          </div>
-          <div className="card h-[200px] p-[15px] bg-[#f0f4f9] rounded-[10px] relative cursor-pointer  hover:bg-[#dfe4ea]">
-            <p className="text-[#585858] text-[17px]">
-              Brainstrom team bonding activities for our work retreat
-            </p>
-            <i class="ri-chat-4-line w-[40px] p-[3px] absolute bg-white rounded-full bottom-[10px] right-[10px] text-2xl text-center"></i>
-          </div>
-          <div className="card h-[200px] p-[15px] bg-[#f0f4f9] rounded-[10px] relative cursor-pointer  hover:bg-[#dfe4ea]">
-            <p className="text-[#585858] text-[17px]">
-              Improve the readability of the following code
-            </p>
-            <i class="ri-code-s-slash-line w-[40px] p-[3px] absolute bg-white rounded-full bottom-[10px] right-[10px] text-2xl text-center"></i>
-          </div>
-        </div>
+        )}
 
         <div className="mainbottom absolute bottom-0 w-[100%] max-w-[900px] m-auto">
           <div className="searchbox flex items-center justify-between gap-[20px] rounded-[50px] bg-[#f0f4f9] px-[20px] py-[10px]">
             <input
+              onChange={(e) => setInput(e.target.value)}
+              value={input}
               className="flex-1 bg-transparent border-none outline-none text-[18px] p-[8px]"
               type="text"
               placeholder="Enter a prompt here"
@@ -54,7 +92,10 @@ function Main() {
             <div className="flex items-center gap-[12px]">
               <i className="ri-gallery-fill w-[24px] cursor-pointer text-xl mr-1"></i>
               <i class="ri-mic-fill w-[24px] cursor-pointer text-xl mr-1"></i>
-              <i class="ri-send-plane-fill w-[24px] cursor-pointer text-xl mr-1"></i>
+              <i
+                onClick={() => onSent()}
+                class="ri-send-plane-fill w-[24px] cursor-pointer text-xl mr-1"
+              ></i>
             </div>
           </div>
           <p className="bottom-info text-[13px] my-[15px] mx-auto text-center font-[300] ">
--- src/context/context.jsx
+++ src/context/context.jsx
@@ -1,15 +1,51 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import run from "../config/Gemini";
 
 export const Context = createContext();
 
 const ContextProvider = (props) => {
   const onSent = async (prompt) => {
-    await run(prompt);
+    setresultData("");
+    setloading(true);
+    setshowResults(true);
+    setrecentPrompt(input);
+    const response = await run(input);
+    let responsearray = response.split("**");
+    let newArray;
+    for (let i = 0; i < responsearray.length; i++) {
+      if (i === 0 || i % 2 !== 1) {
+        newArray += responsearray[i];
+      } else {
+        newArray += "<b>" + responsearray[i] + "</b>";
+      }
+    }
+
+    let newResponse2 = newArray.split("*").join("</br>");
+
+    console.log(responsearray);
+    setresultData(newResponse2);
+    setloading(false);
+    setInput("");
   };
 
-  onSent("what is react js");
-  const contextValue = {};
+  const [input, setInput] = useState("");
+  const [recentPrompt, setrecentPrompt] = useState("");
+  const [previousResponse, setpreviousResponse] = useState([]);
+  const [showResults, setshowResults] = useState(false);
+  const [loading, setloading] = useState(false);
+  const [resultData, setresultData] = useState(false);
+  const contextValue = {
+    previousResponse,
+    setpreviousResponse,
+    onSent,
+    setrecentPrompt,
+    recentPrompt,
+    showResults,
+    loading,
+    resultData,
+    input,
+    setInput,
+  };
 
   return (
     <Context.Provider value={contextValue}>{props.children}</Context.Provider>
