55b3a8b13a64818f518de18991e16ca5ab7c9fd1
Dipanker Sharma <dipankar.kr2005@gmail.com>
2024-06-27 19:46:47 +0530

    Completed

--- src/Component/sidebar/Sidebar.jsx
+++ src/Component/sidebar/Sidebar.jsx
@@ -1,7 +1,15 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
+import { Context } from "../../context/context";
 
 const Sidebar = () => {
   const [extend, setextend] = useState(true);
+  const { onSent, setrecentPrompt, previousResponse, clearAll } =
+    useContext(Context);
+
+  const loadingPrompt = async (prompt) => {
+    setrecentPrompt(prompt);
+    await onSent(prompt);
+  };
 
   return (
     <div className="sidebar min-h-[100vh] inline-flex flex-col justify-between bg-[#f0f4f9] py-[25px] px-[15px]">
@@ -13,17 +21,24 @@ const Sidebar = () => {
         ></i>
         <div className="new-chat inline-flex items-center gap-[10px] px-[10px] py-[10px] mt-2 bg-[#e6eaf1] rounded-[50px] text-[14px] text-grey cursor-pointer ">
           <i className="ri-add-line text-xl"></i>
-          {extend ? <p>New Chat</p> : null}
+          {extend ? <p onClick={() => clearAll()}>New Chat</p> : null}
         </div>
         {extend ? (
           <div className="recent flex flex-col">
             <p className="recent-title mt-[30px] mb-[20px] font-semibold">
               Recent
             </p>
-            <div className="recent-entry flex items-start gap-[10px] px-[20px] cursor-pointer text-[#282828] rounded-[50px] hover:bg-[#e2e6eb] duration-300">
-              <i className="ri-chat-4-line text-lg "></i>
-              <p>What is react...</p>
-            </div>
+            {previousResponse.map((items, index) => {
+              return (
+                <div
+                  onClick={() => loadingPrompt(items)}
+                  className="recent-entry flex items-start gap-[10px] px-[20px] cursor-pointer text-[#282828] rounded-[50px] hover:bg-[#e2e6eb] duration-300"
+                >
+                  <i className="ri-chat-4-line text-lg "></i>
+                  <p>{items.slice(0, 18)}...</p>
+                </div>
+              );
+            })}
           </div>
         ) : null}
       </div>
--- src/context/context.jsx
+++ src/context/context.jsx
@@ -4,14 +4,39 @@ import run from "../config/Gemini";
 export const Context = createContext();
 
 const ContextProvider = (props) => {
+  const [input, setInput] = useState("");
+  const [recentPrompt, setrecentPrompt] = useState("");
+  const [previousResponse, setpreviousResponse] = useState([]);
+  const [showResults, setshowResults] = useState(false);
+  const [loading, setloading] = useState(false);
+  const [resultData, setresultData] = useState("");
+  const clearAll = () => {
+    setloading(false);
+    setshowResults(false);
+  };
+
   const onSent = async (prompt) => {
+    const delayPara = (index, nextWord) => {
+      setTimeout(function () {
+        setresultData((prev) => prev + nextWord);
+      }, 75 * index);
+    };
+
     setresultData("");
     setloading(true);
     setshowResults(true);
-    setrecentPrompt(input);
-    const response = await run(input);
+    let response;
+    if (prompt !== undefined) {
+      response = await run(prompt);
+      setrecentPrompt(prompt);
+    } else {
+      setpreviousResponse((prev) => [...previousResponse, input]);
+      setrecentPrompt(input);
+      response = await run(input);
+    }
+
     let responsearray = response.split("**");
-    let newArray;
+    let newArray = "";
     for (let i = 0; i < responsearray.length; i++) {
       if (i === 0 || i % 2 !== 1) {
         newArray += responsearray[i];
@@ -21,19 +46,17 @@ const ContextProvider = (props) => {
     }
 
     let newResponse2 = newArray.split("*").join("</br>");
-
+    let newResponseArray = newResponse2.split(" ");
+    console.log(newResponseArray);
+    for (let i = 0; i < newResponseArray.length; i++) {
+      const nextWord = newResponseArray[i];
+      delayPara(i, nextWord + " ");
+    }
     console.log(responsearray);
-    setresultData(newResponse2);
     setloading(false);
     setInput("");
   };
 
-  const [input, setInput] = useState("");
-  const [recentPrompt, setrecentPrompt] = useState("");
-  const [previousResponse, setpreviousResponse] = useState([]);
-  const [showResults, setshowResults] = useState(false);
-  const [loading, setloading] = useState(false);
-  const [resultData, setresultData] = useState(false);
   const contextValue = {
     previousResponse,
     setpreviousResponse,
@@ -45,8 +68,8 @@ const ContextProvider = (props) => {
     resultData,
     input,
     setInput,
+    clearAll,
   };
-
   return (
     <Context.Provider value={contextValue}>{props.children}</Context.Provider>
   );
